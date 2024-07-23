eaaa4cbc233c684be90e3263d03135984c699a94
ysh <yang_si_hang@126.com>
2024-07-23 17:57:01 +0800

    202407231756-PD1D

--- Test branch/PD/PD1D.py
+++ Test branch/PD/PD1D.py
@@ -15,4 +15,22 @@ class PD1D:
         self.dt = 1./100
         self.rho = 1.e3
         self.positional_weight = 1.e4
+
+        self.PARTICLE_NUM = 
+
+        self.node_pos = ti.Vector.field(2, dtype=ti.f64, shape=self.PARTICLE_NUM)
+        self.node_pos_init = ti.Vector.field(2, dtype=ti.f64, shape=self.PARTICLE_NUM)
+        self.node_pos_new = ti.Vector.field(self.dim, dtype=ti.f64, shape=self.PARTICLE_NUM)
+        self.node_vel = ti.Vector.field(self.dim, dtype=ti.f64, shape=self.PARTICLE_NUM)
+
+        self.element = ti.Vector.field(2, dtype=ti.i32, shape=self.PARTICLE_NUM-1)
+        self.element_frame = ti.Vector.field(4, dtype=ti.f64, shape=self.PARTICLE_NUM-1)            # 四元数
         
+
+
+def main():
+
+
+
+if __name__ == '__main__':
+    main()
