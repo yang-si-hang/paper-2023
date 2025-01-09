2e00730393ad03f46bc78b9220f7634a6f83a571
yang-si-hang <yang_si_hang@126.com>
2025-01-09 19:30:40 +0800

    202501091930- orbbec point cloud

---
+++ Test branch/Camera/OrbbecShow.py
@@ -0,0 +1,64 @@
+"""Orbbec femto bolt 测试函数
+需要先在terminal中`export PYTHONPATH=$PYTHONPATH:$(pwd)/install/lib/`
+"""
+
+import os, sys
+import cv2
+from pyorbbecsdk import Config
+from pyorbbecsdk import OBError
+from pyorbbecsdk import OBSensorType, OBFormat
+from pyorbbecsdk import Pipeline, FrameSet
+from pyorbbecsdk import VideoStreamProfile
+
+# 设置工作目录为当前脚本所在目录
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)  # 修改当前工作目录
+
+# 添加根目录到 sys.path（跨目录导入模块）
+root_path = os.path.abspath(os.path.join(script_dir, '..'))
+sys.path.append(root_path)
+from Utilize.orbbec_utils import frame_to_bgr_image
+
+ESC_KEY = 27
+
+
+def main():
+    config = Config()
+    pipeline = Pipeline()
+    try:
+        profile_list = pipeline.get_stream_profile_list(OBSensorType.COLOR_SENSOR)
+        try:
+            color_profile: VideoStreamProfile = profile_list.get_video_stream_profile(1280, 0, OBFormat.RBG, 30)
+        except OBError as e:
+            print(e)
+            color_profile = profile_list.get_default_video_stream_profile()
+            print("color profile: ", color_profile)
+        config.enable_stream(color_profile)
+    except Exception as e:
+        print(e)
+        return
+    pipeline.start(config)
+    while True:
+        try:
+            frames: FrameSet = pipeline.wait_for_frames(100)
+            if frames is None:
+                continue
+            color_frame = frames.get_color_frame()
+            if color_frame is None:
+                continue
+            # covert to RGB format
+            color_image = frame_to_bgr_image(color_frame)
+            if color_image is None:
+                print("failed to convert frame to image")
+                continue
+            cv2.imshow("Color Viewer", color_image)
+            key = cv2.waitKey(1)
+            if key == ord('q') or key == ESC_KEY:
+                break
+        except KeyboardInterrupt:
+            break
+    pipeline.stop()
+
+
+if __name__ == "__main__":
+    main()
